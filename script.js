98befdf7ae8540181a044a19402fa5d64d688795
Elena Coffey <34121390+ElenaCoffey@users.noreply.github.com>
2018-04-15 11:41:43 -0700

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Add your javascript here
+$(function(){
+  $("h1").animate({
+    "margin-left": "100px"
+  }, "slow");
+});
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
